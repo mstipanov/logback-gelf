fab57adecea9cf6e7a3e281b0e981c032957502e
Marko Stipanov <marko.stipanov@infobip.com>
2015-12-11 11:38:53 +0100

    1.3.2-infobip - netty TCP sender optimization and debug

--- src/main/java/com/github/pukkaone/gelf/protocol/GelfNettyTCPSender.java
+++ src/main/java/com/github/pukkaone/gelf/protocol/GelfNettyTCPSender.java
@@ -10,15 +10,17 @@ import io.netty.channel.pool.AbstractChannelPoolMap;
 import io.netty.channel.pool.SimpleChannelPool;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.FutureListener;
+import io.netty.util.internal.ConcurrentSet;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Set;
 
 public class GelfNettyTCPSender extends GelfSender {
     private final AbstractChannelPoolMap<InetSocketAddress, SimpleChannelPool> poolMap;
     private final InetSocketAddress address;
     private boolean shutdown;
+    private Set<String> usedChannels = new ConcurrentSet<>();
 
     public GelfNettyTCPSender(int nThreads, String host, int port) throws IOException {
         EventLoopGroup group = new NioEventLoopGroup(nThreads);
@@ -34,6 +36,9 @@ public class GelfNettyTCPSender extends GelfSender {
                 return new SimpleChannelPool(cb.remoteAddress(key), new AbstractChannelPoolHandler() {
                     @Override
                     public void channelCreated(Channel ch) throws Exception {
+                        if (usedChannels.size() < 1000) {
+                            usedChannels.add(ch.toString());
+                        }
 //                        System.out.println("Channel created: " + ch);
                     }
 
@@ -53,6 +58,13 @@ public class GelfNettyTCPSender extends GelfSender {
         address = new InetSocketAddress(host, port);
     }
 
+    public InetSocketAddress getAddress() {
+        return address;
+    }
+
+    public Set<String> getUsedChannels() {
+        return usedChannels;
+    }
 
     @Override
     public boolean sendMessage(final GelfMessage message) {
@@ -60,23 +72,18 @@ public class GelfNettyTCPSender extends GelfSender {
             return false;
         }
 
-        final SimpleChannelPool pool = poolMap.get(address);
-        Future<Channel> f = pool.acquire();
-        f.addListener(new FutureListener<Channel>() {
-            @Override
-            public void operationComplete(Future<Channel> f) {
-                if (f.isSuccess()) {
-                    Channel ch = f.getNow();
-                    ch.write(message.toJson());
-                    ch.writeAndFlush('\0');
-
-                    // Release back to pool
-                    pool.release(ch);
-                }
-            }
-        });
-
-        return true;
+        try {
+            final SimpleChannelPool pool = poolMap.get(address);
+            Future<Channel> f = pool.acquire();
+            Channel ch = f.syncUninterruptibly().getNow();
+            ch.write(message.toJson());
+            ch.writeAndFlush('\0');
+            // Release back to pool
+            pool.release(ch);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
